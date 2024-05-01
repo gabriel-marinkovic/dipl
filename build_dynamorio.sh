1fc042db0bd29ea3207e3e4e9398b89ce9a10e44
Gabriel Marinkovic <gabrielmarink@gmail.com>
2024-05-01 22:17:25 +0200

    Make DynamoRIO debug builds possible

--- build_dynamorio.sh
+++ build_dynamorio.sh
@@ -9,20 +9,25 @@ REPO_URL="https://github.com/DynamoRIO/dynamorio.git"
 REPO_DIR="$SCRIPT_DIR/DynamoRIO_src"
 INSTALL_DIR="$SCRIPT_DIR/DynamoRIO"
 
-rm -rf INSTALL_DIR
+rm -rf "$INSTALL_DIR"
 
 if [[ ! -d "$REPO_DIR" ]]; then
-  git clone -j4 \
-  	--branch master \
-  	--depth 1 --recurse-submodules --shallow-submodules \
-  	"$REPO_URL" "$REPO_DIR"
+    git clone -j4 \
+        --branch release_10.0.0 \
+        --depth 1 --recurse-submodules --shallow-submodules \
+        "$REPO_URL" "$REPO_DIR"
 else
-  echo "Repository directory already exists. Skipping clone."
+    echo "Repository directory already exists. Skipping clone."
 fi
 
 cd "$REPO_DIR"
 rm -rf build
 mkdir build
+
+# DynamoRIO `release_10.0.0` doesn't build with GCC 13 and above without
+# disabling this warning.
+export CFLAGS="${CFLAGS:-} -Wno-dangling-pointer"
+export CXXFLAGS="${CXXFLAGS:-} -Wno-dangling-pointer"
 
 cmake -Bbuild "$@" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake --build build --target install -j
